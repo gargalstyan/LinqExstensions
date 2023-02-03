46be422b5905ed91076b6125dac36c6a2463d34d
Garik Galstyan <garikgalstyan8@gmail.com>
2023-02-03 19:09:48 +0400

    Added some examples

--- LinqExstensions/Program.cs
+++ LinqExstensions/Program.cs
@@ -4,11 +4,21 @@
     {
         static void Main(string[] args)
         {
-            int[] ints = new int[] { 1, 3, 3, 3, -4, -4 };
-            List<int> ints1 = new List<int>() { -2, -1, 4, 1, 6, 7, 8,1,-3,2 };
+            int[] ints = new int[] { 1, 3, 4, 5, 5, 7, 11, 12, 13, -4, -4 };
+            List<int> ints1 = new List<int>() { -2, -1, 4, 1, 6, 7, 8, 1, -3, 2 };
             string[] names = new string[] { "Jhon", "Tom", "Jack" };
             string[] strings = new string[] { "jhon", "Gago", "Hamo" };
-            foreach (var item in ints1.SkipWhile(i => i < 2))
+            Console.WriteLine("All");
+            Console.WriteLine(ints1.MyAll(i=>i>-4));
+            Console.WriteLine("///////////////////////////");
+            Console.WriteLine("Any");
+            Console.WriteLine(ints1.MyAny(i=>i>111));
+            Console.WriteLine("//////////////////////////");
+            Console.WriteLine("Single");
+            Console.WriteLine(ints1.MySingle(i => i > 7));
+            Console.WriteLine("////////////////////////////");
+            Console.WriteLine("Except");
+            foreach (var item in ints.MyExcept(ints1))
             {
                 Console.WriteLine(item);
             }
